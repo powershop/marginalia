816dcfb5405e9804129684392b0988a09dfced7a
Charles Okara <cgokara@gmail.com>
2012-07-16 18:11:46 -0400

    Calling update! around all annotated calls to ensure lines present outside of ActionController contexts

--- lib/marginalia.rb
+++ lib/marginalia.rb
@@ -9,14 +9,24 @@ module Marginalia
       Marginalia::Comment.components = [:application, :controller, :action]
       instrumented_class.class_eval do
         if defined? :execute
-          alias_method :execute_without_marginalia, :execute
-          alias_method :execute, :execute_with_marginalia
+          if Marginalia::Comment.components.include? :line
+            alias_method :execute_without_marginalia, :execute
+            alias_method :execute, :execute_with_marginalia_lines
+          else
+            alias_method :execute_without_marginalia, :execute
+            alias_method :execute, :execute_with_marginalia
+          end
         end
       end
     end
 
     def execute_with_marginalia(sql, name = nil)
       execute_without_marginalia("#{sql} /*#{Marginalia::Comment.to_s}*/", name)
+    end
+
+    def execute_with_marginalia_lines(sql, name = nil)
+      Marginalia::Comment.update!
+      execute_with_marginalia(sql, name)
     end
   end
 
